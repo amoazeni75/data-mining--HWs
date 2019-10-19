1fc1b77c0d355d214e0e29978944b02e02332946
Alireza <alisampo1375@gmail.com>
2019-10-19 18:45:55 +0330

    preparing data and adding to database done!

--- 9423110_HW1_cubes.py
+++ 9423110_HW1_cubes.py
@@ -1,42 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 15 14:21:12 2019
-
 @author: S.Alirzea Moazeni
-
-Datamining Homework 1
+Data mining Homework 1
 """
-
-from __future__ import print_function
+import olap_cubes.prepare_database as pdata
 from mysql import connector
-import pandas as pd
 
-print("inserting the data into the database...")
-
-# 1. Prepare SQL data in memory
-
-mydb = connector.connect(
-  host="localhost",
-  user="root",
-  passwd="",
-  database="olapdb"
-)
-
-london12 = pd.read_csv('london12.csv')
-country_continent = pd.read_csv('countries_by_continent.csv')
-
-# we remove any word of this collection from the name of countries
-removeWords = ["people's", "republic", "of", "democratic", "and"]
-
-
-mycursor = mydb.cursor()
-for index, row in london12.iterrows():
-#    sql = "INSERT INTO london12 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-#    val = (row["continent"], row["country"], row["gender"], row["agegroup"],
-#           row["sport"], row["gold"], row["silver"], row["bronze"])
-#    mycursor.execute(sql, val)
-    country = row["Country"]
+def main():
+    my_db = connector.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="olapdb"
+    )
+    pdata.insert_data_into_database(my_db)
     
-    print(country)   
-
-mydb.commit()
+    
+if __name__ == '__main__':
+    main()
